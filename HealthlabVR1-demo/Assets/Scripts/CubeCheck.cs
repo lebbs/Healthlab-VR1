17548734bf063e4fbdaa63da02c2b7300852939d
joonas <lebbis63@gmail.com>
2020-06-04 16:39:58 +0300

    Kuutiopeli aloitettu
    Aloitettu kuutiopeliä jossa täytyy laittaa kuutiot oikeisiin kohtiin värien perusteella

---
+++ HealthlabVR1-demo/Assets/Scripts/CubeCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject UiObject;
+    public GameObject Trigger;
+
+    private void Start()
+    {
+        UiObject.SetActive(false);
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="RedCube")
+        {
+            UiObject.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UiObject.SetActive(false);
+    }
+}
