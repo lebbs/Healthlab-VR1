e704b638ad4b988f65837104250fa8e7caf8dfcc
joonas <lebbis63@gmail.com>
2020-06-05 15:40:09 +0300

    Palikkapeli ja heittopeli
    Palikkapeliä jatkettu. Toimii, mutta palikoiden asettelu vaatii hienosäätöä. Colliderit menevät toistensa läpi.
    
    Pallopelissä pystyy heittämään pallon maalitauluun ja siihen jää merkki. Tee pistetaulukko ja laita lisää palloja seuraavaksi.

---
+++ HealthlabVR1-demo/Assets/Scripts/CubeGame/CubeCheckGreen.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCheckGreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Punainen palikka
+    public GameObject UiObjectRight;
+    public GameObject UiObjectWrong;
+    public GameObject Trigger;
+
+    ////Vihreä palikka
+    //public GameObject UiObjectRight1;
+    //public GameObject UiObjectWrong1;
+    //public GameObject Trigger1;
+
+    ////Oranssi palikka
+    //public GameObject UiObjectRight2;
+    //public GameObject UiObjectWrong2;
+    //public GameObject Trigger2;
+
+    private void Start()
+    {
+        UiObjectRight.SetActive(false);
+        UiObjectWrong.SetActive(false);
+
+
+        //UiObjectRight1.SetActive(false);
+        //UiObjectWrong1.SetActive(false);
+
+        //UiObjectRight2.SetActive(false);
+        //UiObjectWrong2.SetActive(false);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        StartCoroutine(ShowAndHide(UiObjectRight, 1.0f));
+        //StartCoroutine(ShowAndHide(UiObjectWrong, 1.0f));
+
+        if (other.tag == "GreenCube")
+        {
+            UiObjectRight.SetActive(true);
+
+        }
+        else
+        {
+            UiObjectRight.SetActive(false);
+            UiObjectWrong.SetActive(true);
+        }
+        //if (other.tag == "GreenCube")
+        //{
+        //    UiObjectRight.SetActive(true);
+        //}
+        //else
+        //{
+        //    UiObjectWrong.SetActive(true);
+        //}
+
+        //if (other.tag == "OrangeCube")
+        //{
+        //    UiObjectRight.SetActive(true);
+        //}
+        //else
+        //{
+        //    UiObjectWrong.SetActive(true);
+        //}
+
+
+    }
+
+    IEnumerator ShowAndHide(GameObject gameObject, float delay)
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        UiObjectRight.SetActive(false);
+        UiObjectWrong.SetActive(false);
+
+        //UiObjectRight1.SetActive(false);
+        //UiObjectWrong1.SetActive(false);
+
+        //UiObjectRight2.SetActive(false);
+        //UiObjectWrong2.SetActive(false);
+    }
+}
---
+++ HealthlabVR1-demo/Assets/Scripts/HideText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideText : MonoBehaviour
+{
+
+    public GameObject Text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showtext();
+    }
+
+    void showtext()
+    {
+        StartCoroutine(ShowAndHide(Text, 3.0f));
+
+        Text.SetActive(true);
+
+    }
+
+
+    IEnumerator ShowAndHide(GameObject gameObject, float delay)
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+
+
+}
