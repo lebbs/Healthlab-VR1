99f2edcaaa539d18f7409aaabb137443285fd1f3
joonas <lebbis63@gmail.com>
2020-06-08 16:11:52 +0300

    Pelejä jatkettu ja kauppa simu aloitettu

---
+++ HealthlabVR1-demo/Assets/position.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position : MonoBehaviour
+{
+
+    Vector3 OriginalPosition;
+    void Start()
+    {
+        OriginalPosition = transform.position;
+    }
+
+    public void Respawn()
+    {
+
+        if((transform.position - OriginalPosition).magnitude > 0.5f) {
+            transform.position = OriginalPosition;
+
+        }
+    }
+}
