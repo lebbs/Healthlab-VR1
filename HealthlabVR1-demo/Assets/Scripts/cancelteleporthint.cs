9913e8e733b863baf16096a2750ad108602caac3
joonas <lebbis63@gmail.com>
2020-06-12 14:20:09 +0300

    päivittelyä

---
+++ HealthlabVR1-demo/Assets/Scripts/cancelteleporthint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class cancelteleporthint : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Teleport.instance.CancelTeleportHint();
+    }
+}
