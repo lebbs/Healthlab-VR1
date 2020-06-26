7b9948c797a2fce51b4f67ba74cf8d2056d88fad
joonas <lebbis63@gmail.com>
2020-06-26 10:05:44 +0300

    varmuuskopio

---
+++ HealthlabVR1-demo/Assets/ExitPollHolderTest.cs
@@ -0,0 +1,17 @@
+﻿using CognitiveVR;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPollHolderTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ExitPollSet questionSet;
+
+    void Start()
+    {
+       CognitiveVR.ExitPoll.NewExitPoll("arvosana").Begin();
+    }
+
+    
+}
